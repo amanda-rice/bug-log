5c135299a7d8c22093d1791f754f3b0b8f1c21ac
amanda-rice <aricericerice@gmail.com>
2021-08-13 15:24:43 -0600

    passes first test!

---
+++ buglog/server/controllers/BugsController.js
@@ -0,0 +1,23 @@
+import BaseController from '../utils/BaseController'
+import { Auth0Provider } from '@bcwdev/auth0provider'
+import { bugsService } from '../services/BugsService'
+
+export class BugsController extends BaseController {
+  constructor() {
+    super('api/bugs')
+    this.router
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.create)
+  }
+
+  async create(req, res, next) {
+    try {
+      // do not trust the client
+      req.body.creatorId = req.userInfo.id
+      const bug = await bugsService.create(req.body)
+      res.send(bug)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
--- buglog/server/db/DbContext.js
+++ buglog/server/db/DbContext.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose'
 import { Value as ValueSchema } from '../models/Value'
 import { AccountSchema } from '../models/Account'
+import { Bug } from '../models/Bug'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Bugs = mongoose.model('Bug', Bug)
 }
 
 export const dbContext = new DbContext()
